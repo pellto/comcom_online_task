ed0c7b06b4dd76810951e7c7186427cda829d276
pellto <lovegkgns@gmail.com>
2021-04-28 23:05:57 +0900

    simplify front and back

--- chat_server.py
+++ chat_server.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask, render_template, request
 import requests as module_request
 
 
 app = Flask(__name__, static_url_path='/static')
-POST_URL = "http://203.246.112.132:25000"
+POST_URL = f"http://{os.environ['API_PORT_5001_TCP_ADDR']}:5001"
 
 previous_bot = []
 USER = []
@@ -11,10 +12,14 @@ BOT = []
 
 
 def response_parser(response, user_index=0):
-    start = user_index + response[user_index:].index("<s> ") + 4
-    end = start + min(response[start:].index('<s>'), response[start:].index('</s>'))
+    start = response.index("<s>") + 4
+    end = response[start:].index("</s>") + start
     return response[start:end]
 
+def chatbot_parser(response, user_index=0):
+    start = response.index("</s>") + 5
+    end = response[start:].index("<s>") + start
+    return response[start:end]
 
 @app.route('/')
 def main_get(input_text=None):
@@ -25,49 +30,47 @@ def main_get(input_text=None):
 def KoGPT2_demo(input_text=None):
     if request.method == 'GET':
         input_text = request.args.get('input_text')
-        _type = request.args.get('_type')
         length = request.args.get('length')
-        params = {'input_text': input_text, "do_sample":_type}
+        params = {'input_text': input_text}
         res = module_request.post(url=POST_URL + f"/Original/{length}", json=params)
         status_code = res.status_code
         if status_code == 200:
             res = res.json()
         else:
-            return render_template('KoGPT_api_demo.html', input_text=input_text, _type=str(_type), result=res)
-    return render_template('KoGPT_api_demo.html', input_text=input_text, _type=str(_type), result=res)
+            return render_template('KoGPT_api_demo.html', input_text=input_text, result=res)
+    return render_template('KoGPT_api_demo.html', input_text=input_text, result=res)
 
 
 @app.route('/QnAService', methods=['GET'])
 def QnAService(question=None):
     if request.method == 'GET':
         question = request.args.get('question')
-        _type = request.args.get('_type')
-        params = {'input_text': question, "do_sample": _type}
-        res = module_request.post(url=POST_URL + f"/QnA/{_type}", json=params)
+        params = {'input_text': question}
+        res = module_request.post(url=POST_URL + "/QnA", json=params)
         status_code = res.status_code
         if status_code == 200:
             res = response_parser(res.json())
         else:
-            return render_template('KoGPT_api_demo.html', input_text=question, _type=str(_type), result="")
-        return render_template('QnAService.html', question=question, _type=_type, result=res)
+            return render_template('QnAService.html', question=question, result="")
+        return render_template('QnAService.html', question=question, result=res)
 
 
 @app.route('/QnAChatbot', methods=['GET'])
 def QnAChatbot(user_input=None):
     if request.method == 'GET':
-        bot_answer = request.args.get('bot_answer')
         user_input = request.args.get('user_input')
-        _input = f"{previous_bot[0]}<s>{user_input}" if len(USER) > 1 else f"{user_input}"
+        _input = f"{previous_bot[0]} {user_input}" if len(previous_bot) >= 1 else f"{user_input}"
+        print(_input)
         USER.append(user_input)
-        if len(previous_bot) > 0:
-            params = {'input_text': _input, 'early_stop': False}
-        else:
-            params = {'input_text': _input, 'early_stop': False}
-        res = module_request.post(url=POST_URL+f"/Service/{bot_answer}", json=params)
+        params = {'input_text': _input, 'early_stop': False}
+        res = module_request.post(url=POST_URL+"/Chatbot", json=params)
+        print(res.json())
         status_code = res.status_code
-
         if status_code == 200:
-            res = response_parser(res.json(), len(_input)) if len(BOT) > 0 else response_parser(res.json())
+            try:
+                res = chatbot_parser(res.json())
+            except:
+                res = response_parser(res.json())
             BOT.append(res)
         else:
             return render_template('QnAChatbot.html', history_length=0, user=[], bot=[], current_input='')
